2dfe9a4d5bad16b63cc89a447b3d39a2f9034f65
Fernando <fernando.cortes@ewally.com.br>
2024-10-15 00:03:29 -0300

    :test_tube: test: Criando base de testes

---
+++ backend/test/tests/listSurveyAnswersTest.ts
@@ -0,0 +1,16 @@
+
+
+describe('List Survey answers Testes', () => {
+  it('Should list a survey answers non ordered', function () {
+    const allGood = true
+    allGood.should.be.true()
+  })
+  it('Should list a survey answers ordered by asc', function () {
+    const allGood = true
+    allGood.should.be.true()
+  })
+  it('Should list a survey answers ordered by desc', function () {
+    const allGood = true
+    allGood.should.be.true()
+  })
+})
