30753658217fde36aa0347510de389daef3c7c77
Fernando <fernando.cortes@ewally.com.br>
2024-10-16 21:43:41 -0300

    :bug: fix: Corrige async

--- backend/src/config/db/dbConnect.ts
+++ backend/src/config/db/dbConnect.ts
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
 
 export class Database {
-  public static connect(dbConnString: string): void {
-    mongoose.connect(dbConnString)
+  public static async connect(dbConnString: string): Promise<void> {
+    console.log('Connecting to MongoDB...');
+    return mongoose.connect(dbConnString)
       .then(() => console.log('MongoDB connected'))
       .catch((error) => console.error('Error connecting to MongoDB:', error));
   };
   public static async disconnect(): Promise<void> {
-    try {
-      await mongoose.disconnect();
-      return console.log('MongoDB disconnected');
-    } catch (error) {
-      return console.error('Error disconnecting from MongoDB:', error);
-    }
+    console.log('Disconnecting from MongoDB...');
+    return mongoose.disconnect()
+      .then(() => console.log('MongoDB disconnected'))
+      .catch((error) => console.error('Error disconnecting from MongoDB:', error));
   };
 };
